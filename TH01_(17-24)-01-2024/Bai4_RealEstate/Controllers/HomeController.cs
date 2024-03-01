59298c892951796b5fcf13762948cede5786201c
Tun To Mo <116370663+tun784@users.noreply.github.com>
2024-03-02 00:29:42 +0700

    02-03-2024 00:29

---
+++ TH01_(17-24)-01-2024/Bai4_RealEstate/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai4_RealEstate.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Support()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+	}
+}
