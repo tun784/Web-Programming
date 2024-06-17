486696b2912472f9304e52594934bba61778e8e3
tun784 <thudientu782004@gmail.com>
2024-06-17 17:21:04 +0700

    17-06-2024 17:21

---
+++ KiemTraGiuaKy_Lan2/KiemTraGiuaKyLan2/Controllers/TinTucController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTraGiuaKyLan2.Models;
+
+namespace KiemTraGiuaKyLan2.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: TinTuc
+        KTLan2_DTDDDataContext db = new KTLan2_DTDDDataContext();
+        public ActionResult TinTucTheoCD(int MLTin)
+        {
+            var tinTucTheoCD = db.TinTucs.Where(s => s.MLTin == MLTin).OrderBy(s => s.MLTin).ToList();
+
+            ViewBag.TenTT = db.TinTucs.FirstOrDefault(s => s.MLTin == MLTin).LoaiTin.TLTin.ToString();
+
+            return View(tinTucTheoCD);
+        }
+        public ActionResult TinTucInfo()
+        {
+            return View(db.TinTucs.ToList());
+        }
+    }
+}
---
+++ KiemTraGiuaKy_Lan2/KiemTraGiuaKyLan2/Controllers/TinTucPartialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTraGiuaKyLan2.Models;
+
+namespace KiemTraGiuaKyLan2.Controllers
+{
+    public class TinTucPartialController : Controller
+    {
+        // GET: TinTucPartial
+        KTLan2_DTDDDataContext db = new KTLan2_DTDDDataContext();
+        public ActionResult TinTucPartial()
+        {
+            var listTinTuc = db.LoaiTins.Take(5).ToList();
+            return View(listTinTuc);
+        }
+    }
+}
